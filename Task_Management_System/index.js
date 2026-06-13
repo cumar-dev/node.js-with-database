3ace6840e118d4161a2591cd7d848b1e8d902575
cumar-dev <your_email@example.com>
2026-06-13 23:16:49 +0300

    added middleware used for security

--- Task_Management_System/index.js
+++ Task_Management_System/index.js
@@ -1,6 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
+import hpp from "hpp";
+import compression from "compression";
 import { errorHandler } from "./Middlewares/errorHandler.js";
 import authRout from "./Router/Auth.js";
 import adminRout from "./Router/Admin.js";
@@ -10,10 +18,26 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, process.env.LOCAL_HOST],
+  credentials: true
+}));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 100
+});
+app.use(limiter);
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+app.use(compression());
 app.use("/auth", authRout);
 app.use("/admin", adminRout);
 app.use("/upload", uploadRout);
-app.use("/tasks", taskRout)
+app.use("/tasks", taskRout);
 app.use(errorHandler);
 mongoose
   .connect(process.env.MONGO_URL)
